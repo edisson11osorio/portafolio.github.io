fa67e9b41b98c43216d56339f96882a17ee602c8
edisson11osorio <osorioedisson08@gmail.com>
2023-01-12 23:37:58 -0500

    fix

---
+++ script.js
@@ -0,0 +1,10 @@
+// funtion 
+
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
