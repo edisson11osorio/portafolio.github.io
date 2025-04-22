d1df38fcf764c61778f6fb43c49909c844c84989
Edisson <osorioedisson08@gmail.com>
2025-04-22 15:27:52 -0500

    Add new page certification✅

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function responsiveMenu(){
     var x = document.getElementById("nav");
-    if(x.className ==="responsive"){
+    if(x.className ===""){
         x.className = "responsive";
     }else{
         x.className = "";
